cc627caf7907a639a9322c0ae113dcac4ab204c1
Darshan Pawar <darshanpawar010@gmail.com>
2024-07-27 02:39:04 +0530

    nextjs 14 crud app

---
+++ app/api/prompt/[id]/route.js
@@ -0,0 +1,43 @@
+// GET
+import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
+
+export const GET = async (request, { params }) => {
+  try {
+    await connectToDB();
+    const prompt = await Prompt.findById(params.id).populate("creator");
+    if (!prompt) return new Response("Prompt not found", { status: 400 });
+    return new Response(JSON.stringify(prompt), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to fetch the prompt", { status: 500 });
+  }
+};
+
+// PATCH
+
+export const PATCH = async (request, { params }) => {
+  const { prompt, tag } = await request.json();
+  try {
+    await connectToDB();
+    const existingPrompt = await Prompt.findById(params.id).populate("creator");
+    if (!prompt) return new Response("Prompt not found", { status: 400 });
+    existingPrompt.prompt = prompt;
+    existingPrompt.tag = tag;
+    await existingPrompt.save();
+    return new Response(JSON.stringify(existingPrompt), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to update the prompt", { status: 500 });
+  }
+};
+
+// DELETE
+
+export const DELETE = async (request, { params }) => {
+  try {
+    await connectToDB();
+    await Prompt.findByIdAndDelete(params.id).populate("creator");
+    return new Response("Prompt deleted successfully", { status: 200 });
+  } catch (error) {
+    return new Response("Failed to delete the prompt", { status: 500 });
+  }
+};
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "**",
+      },
+    ],
+    // domains: ["lh3.googleusercontent.com"],
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
